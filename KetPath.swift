2a3406b5918ddc7d47a81b03e3cd97e203653dec
Ben Snitkoff <ben@snitkoff.com>
2021-04-19 18:27:04 -0400

    first addition to keypath

---
+++ KetPath.swift
@@ -0,0 +1,22 @@
+struct SomeStructure {
+    var someValue: Int
+}
+
+let s = SomeStructure(someValue: 12)
+let pathToProperty = \SomeStructure.someValue
+
+let value = s[keyPath: pathToProperty]
+// value is 12
+The type name can be omitted in contexts where type inference can determine the implied type. The following code uses \.someProperty instead of \SomeClass.someProperty:
+
+class SomeClass: NSObject {
+    @objc dynamic var someProperty: Int
+    init(someProperty: Int) {
+        self.someProperty = someProperty
+    }
+}
+
+let c = SomeClass(someProperty: 10)
+c.observe(\.someProperty) { object, change in
+    // ...
+}
