4cf359d33ede017ffd54d5d417eea38bed944b9e
Ben Snitkoff <ben@snitkoff.com>
2021-04-19 18:07:18 -0400

    getting the hang of property wrappers now

--- PropertyWrappers/PropertyWrappers.swift
+++ PropertyWrappers/PropertyWrappers.swift
@@ -1,27 +1,46 @@
 import Foundation
 
+// The object has the @propertyWrapper prefix when it's defined
+@propertyWrapper
+struct Versioned<Element> {
+    private var currentValue: Element
+    private(set) var history: [Element] = []
+    
+    init(wrappedValue: Element) { currentValue = wrappedValue }
+    
+    //it needs a wrapped value which can modify the stored value or place limits on it.
+    var wrappedValue: Element {
+        get { return currentValue }
+        set {
+            history.append(currentValue)
+            currentValue = newValue
+        }
+    }
+    
+    //ProjectedValue is accessed with a $ before the variable. It can return self to get access to more variables.
+    var projectedValue: [Element] {
+        return history
+    }
+}
+
+struct User {
+    //You can't use property wrappers at the top level or at local levels. They have to be in a struct, enum, or class for now.
+    @Versioned var name: String = ""
+}
+
 main()
 
 
 func main() {
-	@Versioned var names: String = "Ben"
-	names = "James"
-	names = "Edna"
-	names = "Jessica"
-	print($names)
+    var user = User()
+    user.name = "Ben"
+    user.name = "James"
+    user.name = "Edna"
+    user.name = "Jessica"
+    print(user.$name)
 }
 
 
-@propertyWrapper
-struct Versioned<Element> {
-	private var value: Element
-	var projectedValue: [Element]
-	init(_ element: Element) { value = element }
-	var wrappedValue: Element {
-		get { return value }
-		set { 
-			value = newValue
-			projectedValue.append(newValue)
-		}
-	}
-} 
+
+
+
